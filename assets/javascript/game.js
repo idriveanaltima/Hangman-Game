f823b3f2c23b09a5d1168d7735388d906a2c7da0
JB <jmb2883@gmail.com>
2018-03-30 23:03:48 -0400

    adding words to game and updating html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,8 @@
-
 // Declaring variables for the game
 
 var userGuess;
 var lettersGuessed = [];
-var lettersInWord = []
+var lettersInWord = [];
 var currentWord = newWord();
 var guessesRemaining = currentWord.length * 2;
 var lettersMatched = 0;
@@ -15,8 +14,8 @@ var losses = 0;
 
 // this function will take in a number to add the correct number of _'s for the hangman word
 
-function start () {
-  for(i=0;i < currentWord.length;i++) {
+function onPageLoad() {
+  for (i = 0; i < currentWord.length; i++) {
     lettersInWord[i] = "_";
   }
   y = lettersInWord.join(" ")
@@ -31,72 +30,66 @@ function start () {
 // function for determining the randomly selected word from the array and pulling out the letters
 
 function newWord() {
-var words = ["apple", "orange", "banana", "cherry"];
-currentWord = words[Math.floor(Math.random() * words.length)];
-return currentWord;
+  var words = ['acai', 'apples', 'apricots', 'avocado', 'ackee', 'bananas', 'blueberries', 'blackberries', 'cantaloupes', 'cherries', 'cranberries', 'currants', 'dates', 'elderberries', 'figs', 'gooseberries', 'grapefruit', 'kiwis', 'lemons', 'mangos', 'mulberries', 'grapes', 'grapefruits', 'honeydew melons', 'oranges', 'papaya', 'pineapples', 'raspberries', 'peaches'acai'acai', 'apples', 'apricots', 'avocado', 'ackee', 'bananas', 'blueberries', 'blackberries', 'cantaloupes', 'cherries', 'cranberries', 'currants', 'dates', 'elderberries', 'figs', 'gooseberries', 'grapefruit', 'kiwis', 'lemons', 'mangos', 'mulberries', 'grapes', 'grapefruits', 'honeydew melons', 'oranges', 'papaya', 'pineapples', 'raspberries', 'peaches'];
+  currentWord = words[Math.floor(Math.random() * words.length)];
+  return currentWord;
 }
 console.log(currentWord);
 
-function reset () {
-  newWord();
+function reset() {
+  newWord(currentWord);
+  lettersMatched = 0;
+  lettersGuessed = [];
+  guessesRemaining = currentWord.length * 2;
+  lettersInWord = [];
+  for (i = 0; i < currentWord.length; i++) {
+    lettersInWord[i] = "_";
+  }
+  y = lettersInWord.join(" ")
+  document.querySelector("#word").innerHTML = y;
   document.querySelector("#guessesLeft").innerHTML = guessesRemaining;
+  document.querySelector("#lettersUsed").innerHTML = lettersGuessed;
 }
 
 // function for determining the sequence of events when a user enters a letter
 
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
   userGuess = event.key;
-  
-//will validate the letter to see if it was a valid letter and validate whether a letter has already been pressed.
 
- if (/^[a-z]/.test(userGuess) === true) {
-      lettersGuessed.push(userGuess);
-      document.querySelector("#lettersUsed").innerHTML = lettersGuessed;
-    }else {
-      return
-}
+  //will validate the letter to see if it was a valid letter and validate whether a letter has already been pressed.
 
-if (currentWord.indexOf(userGuess) > -1) {
-  console.log(currentWord.indexOf(userGuess));
-  for (i=0; i < currentWord.length; i++) {
-   if (currentWord[i] == userGuess) {
-    console.log(currentWord.indexOf(userGuess));
-          lettersMatched++;
-          console.log(lettersMatched);
-          lettersInWord[i] = userGuess;
-          console.log(lettersInWord[i]);
-          document.querySelector("#word").innerHTML = lettersInWord.join(" ");
+
+
+  if (/^[a-z]/.test(userGuess) === true) {
+    lettersGuessed.push(userGuess);
+    document.querySelector("#lettersUsed").innerHTML = lettersGuessed;
+  } else {}
+
+  if (currentWord.indexOf(userGuess) > -1) {
+    for (i = 0; i < currentWord.length; i++) {
+      if (currentWord[i] == userGuess) {
+        lettersMatched++;
+        console.log(lettersMatched);
+        lettersInWord[i] = userGuess;
+        document.querySelector("#word").innerHTML = lettersInWord.join(" ");
       }
     }
   } else {
-  console.log(currentWord.indexOf(userGuess))
-   guessesRemaining--;
-   document.querySelector("#guessesLeft").innerHTML = guessesRemaining;
- }
+    console.log(currentWord.indexOf(userGuess))
+    guessesRemaining--;
+    document.querySelector("#guessesLeft").innerHTML = guessesRemaining;
+  }
 
- 
-
-if (lettersMatched === currentWord.length){
+  if (lettersMatched === currentWord.length) {
     wins++;
     document.querySelector("#wins").innerHTML = wins;
     console.log("game over");
-    reset ();
-    } else {
-      return;
-    }
-    
-if (guessesRemaining === 0){
-      losses++;
-      document.querySelector("#losses").innerHTML = losses;
-    } else {
-        return;
-      }
-  }
+    reset();
+  } else {}
+
+}
 
 
 
-
-document.querySelector("#word").innerHTML = start();
-
-
+document.querySelector("#word").innerHTML = onPageLoad();
