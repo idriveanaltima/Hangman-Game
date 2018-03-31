03440401e34cb8a216abe8d909a51a3f788da80c
JB <jmb2883@gmail.com>
2018-03-31 00:13:51 -0400

    removing comments and logging from code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,14 @@ var userGuess;
 var lettersGuessed = [];
 var lettersInWord = [];
 var currentWord = newWord();
-var guessesRemaining = currentWord.length * 2;
+var guessesRemaining = currentWord.length;
 var lettersMatched = 0;
 var wins = 0;
-var losses = 0;
 
 // FUNCTIONS SECTION
 // ______________________
 
-// this function will take in a number to add the correct number of _'s for the hangman word
+// this function will take in a number to add the correct number of _'s for the hangman word and initalize values
 
 function onPageLoad() {
   for (i = 0; i < currentWord.length; i++) {
@@ -21,26 +20,26 @@ function onPageLoad() {
   y = lettersInWord.join(" ")
   document.querySelector("#guessesLeft").innerHTML = guessesRemaining;
   document.querySelector("#wins").innerHTML = wins;
-  document.querySelector("#losses").innerHTML = losses;
   document.querySelector("#word").innerHTML = y;
   return y;
 }
 
 
-// function for determining the randomly selected word from the array and pulling out the letters
+// function for determining the randomly selected word from the array
 
 function newWord() {
-  var words = ['acai', 'apples', 'apricots', 'avocado', 'ackee', 'bananas', 'blueberries', 'blackberries', 'cantaloupes', 'cherries', 'cranberries', 'currants', 'dates', 'elderberries', 'figs', 'gooseberries', 'grapefruit', 'kiwis', 'lemons', 'mangos', 'mulberries', 'grapes', 'grapefruits', 'honeydew melons', 'oranges', 'papaya', 'pineapples', 'raspberries', 'peaches'];
+  var words = ['acai', 'apples', 'apricots', 'avocado', 'ackee', 'bananas', 'blueberries', 'blackberries', 'cantaloupes', 'cherries', 'cranberries', 'currants', 'dates', 'elderberries', 'figs', 'gooseberries', 'grapefruit', 'kiwis', 'lemons', 'mangos', 'mulberries', 'grapes', 'grapefruits', 'oranges', 'papaya', 'pineapples', 'raspberries', 'peaches'];
   currentWord = words[Math.floor(Math.random() * words.length)];
   return currentWord;
 }
-console.log(currentWord);
+
+// function for resetting the game when a player wins or is out of guesses
 
 function reset() {
   newWord(currentWord);
   lettersMatched = 0;
   lettersGuessed = [];
-  guessesRemaining = currentWord.length * 2;
+  guessesRemaining = currentWord.length;
   lettersInWord = [];
   for (i = 0; i < currentWord.length; i++) {
     lettersInWord[i] = "_";
@@ -57,39 +56,54 @@ function reset() {
 document.onkeyup = function (event) {
   userGuess = event.key;
 
-  //will validate the letter to see if it was a valid letter and validate whether a letter has already been pressed.
 
+
+  //will validate the letter to see if it was a valid entry before storing in the list
 
 
   if (/^[a-z]/.test(userGuess) === true) {
     lettersGuessed.push(userGuess);
     document.querySelector("#lettersUsed").innerHTML = lettersGuessed;
-  } else {}
+  } else {
+};
 
+//matching the guessed letters to the word or reducing # ofguesses if incorrect
   if (currentWord.indexOf(userGuess) > -1) {
     for (i = 0; i < currentWord.length; i++) {
       if (currentWord[i] == userGuess) {
         lettersMatched++;
-        console.log(lettersMatched);
         lettersInWord[i] = userGuess;
         document.querySelector("#word").innerHTML = lettersInWord.join(" ");
       }
     }
   } else {
-    console.log(currentWord.indexOf(userGuess))
     guessesRemaining--;
     document.querySelector("#guessesLeft").innerHTML = guessesRemaining;
-  }
+  };
+
+  //if the word is complete then add to wins and reset the game
 
   if (lettersMatched === currentWord.length) {
     wins++;
     document.querySelector("#wins").innerHTML = wins;
-    console.log("game over");
     reset();
-  } else {}
+  } else {
 
-}
+  };
+
+  //if there are no more guesses then reset the game
+
+  if (guessesRemaining === 0) {
+    reset()
+  } else {
+
+  };
+
+};
 
 
+
+
+//will call this function to load the page.
 
 document.querySelector("#word").innerHTML = onPageLoad();
